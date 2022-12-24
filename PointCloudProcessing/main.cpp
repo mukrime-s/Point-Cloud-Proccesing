38636ca3c8008e955fea125276ce8a1f3a808554
OmerKurtuldu <85666770+OmerKurtuldu@users.noreply.github.com>
2022-12-24 20:17:12 +0300

    Duzenlemeler yapildi

--- PointCloudProcessing/main.cpp
+++ PointCloudProcessing/main.cpp
@@ -1,26 +1,21 @@
-#include<iostream>
-#include "DepthCamera.h"
-#include "PointCloud.h"
-using namespace std;
-
-int main() {
-
-
-	//DepthCamera dp;
-	//dp.setfileName("omer.txt");
-	//dp.capture();
-	//PointCloud pc;
-	//pc.getPoint(10);
-
-
-
-
-
-
-
-
-
-
-
-	return 0;
-}
+//#include<iostream>
+//#include "DepthCamera.h"
+//#include "PointCloud.h
+//
+//using namespace std;
+//
+//int main() {
+//
+//
+//	
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
