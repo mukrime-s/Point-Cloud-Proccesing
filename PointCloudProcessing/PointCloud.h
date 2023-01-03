e9fc5c0492239d3fd05bc59682cd83f9943df9ec
mükrime sağıroğlu <102221568+mukrime-s@users.noreply.github.com>
2023-01-03 16:58:40 +0300

    erors fixed

--- PointCloudProcessing/PointCloud.h
+++ PointCloudProcessing/PointCloud.h
@@ -12,25 +12,25 @@ using namespace std;
 class PointCloud
 {
 private:
+	int pointNumber;
 	list <Point> points;
 public:
 	PointCloud();
 	~PointCloud();
-	PointCloud(int);//Dizinin boyutu, nesne yaratýlýrken constructor fonksiyonunda bir parametre olarak alýnýr.
 	//Point sýnýfýndan setPoint fonksiyonlarý alýndý
 	void setPointX(int, double);
 	void setPointY(int, double);
 	void setPointZ(int, double);
 	void setPointNumber(int); //point cloudun icindeki pointleri point objesi kullanarak setleyen foksiyon olusturuldu.
-	void setPoint(int, Point); // point classýndaki indexi pointin indexine esitler.
+	void setPoint(list<Point>* pc); // point classýndaki indexi pointin indexine esitler.
 	//Point sýnýfýndan getPoint fonksiyonlarý alýndý sabit kalmasý için const kullanýldý.
-	double getPointX(int)const;
-	double getPointY(int)const;
-	double getPointZ(int)const;
-	Point getPoint(int)const;////point cloudun icindeki pointleri point objesi kullanarak objeye erisen foksiyon olusturuldu.
+	double getPointX(int);
+	double getPointY(int);
+	double getPointZ(int);
+	list<Point> getPoint()const;////point cloudun icindeki pointleri point objesi kullanarak objeye erisen foksiyon olusturuldu.
 	int getPointNumber()const;//pointNumberi döndürür.
-	const PointCloud operator+(PointCloud&); //iki nokta bulutunun sahip olduðu noktalara sahip tek bir nokta bulutunu döndürür.
-	const PointCloud operator=(PointCloud&); //bir nokta bulutunun baþka bir nokta bulutuna kopyalanmasýný saðlar.
+	PointCloud operator+(PointCloud); //iki nokta bulutunun sahip olduðu noktalara sahip tek bir nokta bulutunu döndürür.
+	void operator=(PointCloud *pcl); //bir nokta bulutunun baþka bir nokta bulutuna kopyalanmasýný saðlar.
 
 	PointCloud deleteMovePoint(int);//secilen pointi listeden siler ve silinen pointten sonraki degerleri bir öncekine tasir.
 
